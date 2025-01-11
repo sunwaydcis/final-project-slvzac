5e5f2b5cfbcabeef0cd1c334d34074143369e2ab
Zachary <slvzachary@gmail.com>
2025-01-11 22:02:20 +0800

    Redid Combat Announcement

--- src/main/scala/Monster.scala
+++ src/main/scala/Monster.scala
@@ -1,11 +1,10 @@
 import scala.util.Random
-// Monster AI MOVEMENT
 
 class Monster(var x: Int, var y: Int, var health: Int) {
   def move(dungeon: Dungeon): Unit = {
     // Simple random movement logic (can be improved)
     val directions = List("w", "s", "a", "d")
-    val direction = directions(scala.util.Random.nextInt(directions.length))
+    val direction = directions(Random.nextInt(directions.length))
 
     dungeon.updatePosition(x, y, "_") // Clear current position
 
@@ -20,25 +19,11 @@ class Monster(var x: Int, var y: Int, var health: Int) {
     dungeon.updatePosition(x, y, "M") // Set new position
   }
 
-  def attack(player: Player): Unit = {
-    val damage = 2 + Random.nextInt(9) // RNG dmg between 2 and 10
-    println(s"Monster attacks the player, inflicting $damage damage!")
-    player.health -= damage
-    if (player.health <= 0) {
-      println("Player is defeated!")
-    } else {
-      println(s"Player's health: ${player.health}")
-    }
+  def attack(player: Player): Int = {
+    2 + Random.nextInt(9) // RNG damage between 2 and 10
   }
 
-  def specialAttack(player: Player): Unit = {
-    val damage = 2 + Random.nextInt(9) // RNG
-    println(s"Monster performs a special attack on the player, inflicting $damage damage!")
-    player.health -= 20
-    if (player.health <= 0) {
-      println("Player is defeated!")
-    } else {
-      println(s"Player's health: ${player.health}")
-    }
+  def specialAttack(player: Player): Int = {
+    4 + Random.nextInt(12) // RNG damage between 4 and 15
   }
 }
--- src/main/scala/Player.scala
+++ src/main/scala/Player.scala
@@ -20,88 +20,55 @@ class Player(var x: Int, var y: Int, var health: Int) {
     dungeon.updatePosition(x, y, "P") // Set new position
   }
 
-  def combat(monster: Monster): Unit = {
-    println("Player encounters the monster!")
-    while (health > 0 && monster.health > 0) {
-      println("Choose action: attack, block, special, heal")
-      val action = scala.io.StdIn.readLine()
-      usedPotion = false
-      skipMonsterTurn = false
-      action match {
-        case "attack" => attack(monster)
-        case "block" => blockAttack(monster)
-        case "special" =>
-          if (specialCooldown == 0) {
-            useSpecialAbility(monster)
-            specialCooldown = 3 // Reset cooldown
-          } else {
-            println(s"Special ability is on cooldown for $specialCooldown more turns.")
-            skipMonsterTurn = true
-          }
-        case "heal" => usePotion()
-        case _ => println("Invalid action")
-      }
-      if (monster.health > 0 && !usedPotion && !skipMonsterTurn) {
-        monster.attack(this)
-      }
-      if (specialCooldown > 0 && !skipMonsterTurn) {
-        specialCooldown -= 1 // Decrement cooldown each turn
-      }
-    }
+  def combat(): String = {
+    println("Choose action: attack, block, special, heal")
+    scala.io.StdIn.readLine()
   }
 
-  def attack(monster: Monster): Unit = {
-    val damage = 5 + Random.nextInt(6)// Random damage between 5 and 10
-    println(s"Player attacks the monster, inflicting $damage damage!")
+  def attack(monster: Monster): Int = {
+    val damage = 5 + Random.nextInt(6) // Random damage between 5 and 10
     monster.health -= damage
-    if (monster.health <= 0) {
-      println("Monster is defeated!")
-    } else {
-      println(s"Monster's health: ${monster.health}")
-    }
+    damage
   }
 
-  def blockAttack(monster: Monster): Unit = {
-    println("Player blocks the monster's attack!")
-    // Reduce the damage taken by half
-    val damage = 5
-    health -= damage
-    if (health <= 0) {
-      println("Player is defeated!")
+  def blockAttack(monster: Monster): (Int, Int) = {
+    val damage = if (Random.nextInt(6) == 0) {
+      monster.specialAttack(this)
     } else {
-      println(s"Player's health: ${health}")
+      monster.attack(this)
     }
+    val blockPercentage = Random.nextInt(101) // Random percentage between 0 and 100
+    val blockedDamage = damage * blockPercentage / 100
+    val actualDamage = damage - blockedDamage
+    health -= actualDamage
+    skipMonsterTurn = true
+    (actualDamage, health)
   }
 
-  def useSpecialAbility(monster: Monster): Unit = {
-    val damage = 20 +  Random.nextInt(21)// Random damage between 20 and 40
-    println(s"Player uses a special ability inflicting $damage damage!")
+  def useSpecialAbility(monster: Monster): Int = {
+    val damage = 20 + Random.nextInt(21) // RNG damage between 20 and 40
     monster.health -= damage
-    if (monster.health <= 0) {
-      println("Monster is defeated!")
-    } else {
-      println(s"Monster's health: ${monster.health}")
-    }
+    damage
   }
 
-  def usePotion(): Unit = {
+  def usePotion(): (Boolean, Int) = {
     val potions = inventory.collect { case p: Potion => p }
     potions.headOption match {
       case Some(potion) =>
         println(s"Do you want to use a ${potion.name}? You have ${potions.size} left. (yes/no)")
         val confirmation = scala.io.StdIn.readLine()
         if (confirmation.toLowerCase == "yes") {
-          println(s"Player uses a ${potion.name}!")
           health += potion.healingAmount
           inventory = inventory.filterNot(_ == potion)
           usedPotion = true
-          println(s"Player's health: $health")
+          (true, health)
         } else {
-          println("Potion use canceled.")
           skipMonsterTurn = true
+          (false, health)
         }
       case None =>
         println("No potions left!")
+        (false, health)
     }
   }
 }
